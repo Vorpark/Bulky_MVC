ab0e47230252596fd538f06296f8f76711e910af
Vorpark <nik.vorpark@mail.ru>
2024-01-08 02:59:12 +0300

    Основные настройки EF Core

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
