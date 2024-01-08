31af7077e26eb2af591f8194bfdd3ee81af12afc
Vorpark <nik.vorpark@mail.ru>
2024-01-08 03:15:12 +0300

    Изменение настройки DbContext и применение миграции AddCategories

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BulkyWeb.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Category> Categories { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
             
