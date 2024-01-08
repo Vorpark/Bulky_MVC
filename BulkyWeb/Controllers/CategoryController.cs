902bb3ef780b4b8cbbbaab0ebf39becabacf5511
Vorpark <nik.vorpark@mail.ru>
2024-01-08 05:47:33 +0300

    Отображение категорий  и обновление bootstrap

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            var objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
