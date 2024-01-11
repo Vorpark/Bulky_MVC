c2f203af4fce21229b41192be4222c5a623104a9
Vorpark <nik.vorpark@mail.ru>
2024-01-12 00:55:46 +0300

    Реализация добавление, редактирования и удаления экземпляра категории для RazorPages

---
+++ BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,31 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Category Category { get; set; }
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(Category);
+                _db.SaveChanges();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
