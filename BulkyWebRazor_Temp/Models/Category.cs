8cb6319398a0edb18baac485afe1bf55f254e526
Vorpark <nik.vorpark@mail.ru>
2024-01-11 22:42:11 +0300

    Основные настройки EF Core для RazorPages

---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWebRazor_Temp.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Поле не может быть пустым")]
+        [DisplayName("Имя категории")]
+        [MaxLength(30)]
+        public string Name { get; set; }
+        [DisplayName("Порядок отображения категории")]
+        [Range(1, 100, ErrorMessage = "Допустимый диапазон от 1 до 100")]
+        public int DisplayOrder { get; set; }
+    }
+}
