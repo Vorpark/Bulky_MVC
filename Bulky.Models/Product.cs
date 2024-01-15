f9bd53fd340ef91a1828f19deede1bc93b5ce682
Vorpark <nik.vorpark@mail.ru>
2024-01-15 07:19:32 +0300

    Полная реализация Controller и View для работы с Product

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -8,18 +8,21 @@ namespace Bulky.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Поле не может быть пустым")]
+        [Display(Name = "Титульник")]
         public string Title { get; set; }
 
+        [Display(Name = "Описание")]
         public string Description { get; set; }
 
         [Required(ErrorMessage = "Поле не может быть пустым")]
         public string ISBN { get; set; }
 
         [Required(ErrorMessage = "Поле не может быть пустым")]
+        [Display(Name = "Автор")]
         public string Author { get; set; }
 
         [Required(ErrorMessage = "Поле не может быть пустым")]
-        [Display(Name = "Список цен")]
+        [Display(Name = "Прейскурант")]
         [Range(0, 10000)]
         public double ListPrice { get; set; }
 
---
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,91 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var objProductList = _unitOfWork.Product.GetAll().ToList();
+            return View(objProductList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Продукт успешно создан.";
+                return RedirectToAction("Index", "Product");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var productFromDb = _unitOfWork.Product.Get(x => x.Id == id);
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Продукт успешно изменен.";
+                return RedirectToAction("Index", "Product");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var productFromDb = _unitOfWork.Product.Get(x => x.Id == id);
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int id)
+        {
+            var obj = _unitOfWork.Product.Get(x => x.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Продукт успешно удален.";
+            return RedirectToAction("Index", "Product");
+        }
+    }
+}
